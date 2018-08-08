ca90b3f131b2f34ac2f872a64f479231ce0f185c
Danzilla <Dan.alphonza@gmail.com>
2018-08-08 12:28:23 -0400

    Updated with file upload and add to database

--- app/server/transaction.js
+++ app/server/transaction.js
@@ -1,12 +1,13 @@
 var express = require('express');
-var csvjson = require("csvtojson");
+var moment = require('moment');
+var csvjson = require('csvjson');
 var fs = require('fs');
 
 var router = express.Router();
 
+
 /* GET login page */
 router.get('/', function(req, res, next) {
-
   if(req.session.user == undefined){
     var msg = "Session is empty";
     sessionName = "Session is Empty";
@@ -17,60 +18,138 @@ router.get('/', function(req, res, next) {
     console.log(msg);
   }
 
-  var options = {
-    delimiter : ',' , // optional
-    quote     : '"' // optional
-  };
-  var file_data = fs.readFileSync('air.csv', { encoding : 'utf8'});
-  console.log(file_data);
+  var db = req.db;
+  var collection = db.get('statementCollection');
+  // statement
+  //  - statementInfo
+  //  - transactionInfo
+  //  - dateAdded
 
-  csvjson({
-      noheader:true,
-      output: "csv"
-  })
-  .fromString(file_data)
-  .then((csvRow)=>{
-      console.log(csvRow) // => [["1","2","3"], ["4","5","6"], ["7","8","9"]]
-  })
 
-  var transactionInfo = "asd";
+  var pageInfo = {
+    title: 'upload',
+    page: "page POST",
+    request: "post",
+    sessionName: sessionName
+  }
+  collection.find({ }, function(e, results){
+    res.render('transaction/view', { // user/view
+      transaction: results,
+      pageInfo: pageInfo
+    });
+  });
 
-  res.render('transaction/view', {
-    title: 'transaction GET Home',
-    logM: msg,
-    sessionName: sessionName,
-    transactionInfo: transactionInfo
+});
+
+/* GET upload page */
+router.get('/upload', function(req, res, next) {
+  if(req.session.user == undefined){
+    var msg = "Session is empty";
+    sessionName = "Session is Empty";
+    console.log(msg);
+  } else {
+    var msg = "Session is active, user: " + req.session.user;
+    sessionName = req.session.user;
+    console.log(msg);
+  }
+
+  var pageInfo = {
+    title: 'upload',
+    page: "page GET",
+    request: "get",
+    sessionName: sessionName
+  }
+  res.render('transaction/upload', {
+    pageInfo: pageInfo,
    });
 });
 
+
 /* POST home page. */
-router.post('/', function(req, res, next) {
-    if(req.session.user == undefined){
-      var msg = "Session is empty";
-      sessionName = "Session is Empty";
-      console.log(msg);
-    } else {
-      var msg = "Session is active, user: " + req.session.user;
-      sessionName = req.session.user;
-      console.log(msg);
-    }
-
-  var transactionInfo = {
-    id: req.body.id,
-    date: req.body.Date,
-    Desc: req.body.Desc,
-    Withdraw: req.body.Withdraw,
-    Deposite: req.body.Deposite,
-    Balance: req.body.Balance,
-    Category: req.body.Category,
+router.post('/upload', function(req, res, next) {
+  if(req.session.user == undefined){
+    var msg = "Session is empty";
+    sessionName = "Session is Empty!";
+    console.log(msg);
+  } else {
+    var msg = "Session is active, user: " + req.session.user;
+    sessionName = req.session.user;
+    console.log(msg);
   }
-  console.log("transaction: " + transactionInfo);
 
-  res.render('transaction/view', {
-    title: 'transaction POST Home',
-    logM: msg,
-    sessionName: sessionName,
-    transactionInfo: transactionInfo
+  if (req.body.submitMode === "self"){
+    console.log("YO N");
+    console.log(statementInfo);
+  } else {
+    // Upload object - setting up for Statement and transaction
+    var uploadInfo = {
+      statementInfo : "statementInfo",
+      transactionInfo : "transactionInfo",
+      uploadDate : moment().format('MMMM Do YYYY, h:mm:ss a')
+    };
+    // Statement array from GET upload
+    var statementInfo = {
+      statementName : req.body.statementName,
+      statementType : req.body.statementType,
+      statementDate : req.body.statementDate,
+      statementDesc : req.body.statementDesc,
+      statementFileInput : req.body.statementFileInput
+    };
+    uploadInfo.statementInfo = statementInfo; // inserting to upload object
+    // getting CSV to JSON
+    var csvFile = statementInfo.statementFileInput;
+    var csvData = fs.readFileSync(csvFile, { encoding : 'utf8'});
+    var options = {
+      delimiter : ',', // optional
+      quote     : '"' // optional
+    };
+    // transactionInfo = New array to store json formated transaction
+    var transactionInfo = [];
+    var csvTransactionInfo = csvjson.toArray(csvData, options);
+    for (var i in csvTransactionInfo) {
+      transactionInfo[i] = {
+        transId: i,
+        transDate: csvTransactionInfo[i][0],
+        transDesc: csvTransactionInfo[i][1],
+        transWithdraw: csvTransactionInfo[i][2],
+        transDeposite: csvTransactionInfo[i][3],
+        transBalance: csvTransactionInfo[i][4]
+      }
+    }
+    uploadInfo.transactionInfo = transactionInfo; // inserting to upload object
+  }
+
+
+  if(transactionInfo){console.log("good transactionInfo!");}
+  if(statementInfo){console.log("good statementInfo!");}
+
+  var db = req.db;
+  var collection = db.get('statementCollection');
+  // statementCollection
+  //  - statementInfo
+  //  - transactionInfo
+  //  - dateAdded
+  // Submit to the DB
+  collection.insert(uploadInfo, function (err, results) {
+    if (err) { // If it failed, return error
+      res.send("There was a problem adding the information to the database.");
+    }
+    else { // Hey! We Add new one!
+      console.log(results);
+      console.log("DB GOOD TO Insert");
+    }
+  });
+
+  var pageInfo = {
+    title: 'upload',
+    page: "page POST",
+    request: "post",
+    sessionName: sessionName
+  }
+  res.render('transaction/upload', {
+    transactionInfo: transactionInfo,
+    statementInfo: statementInfo,
+    pageInfo: pageInfo
    });
 });
 
