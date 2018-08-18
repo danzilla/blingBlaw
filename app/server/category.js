d2cd1f958a9c57213a9db4a36ed6081862fed6b1
Danzilla <Dan.alphonza@gmail.com>
2018-08-18 18:54:25 -0400

    Cat is done! Time for Statements #blaw

--- app/server/category.js
+++ app/server/category.js
@@ -20,13 +20,6 @@ router.get('/', function(req, res, next) {
   var db = req.db;
   var collection = db.get('categorycollection');
   collection.find({}, {}, function(e, results) {
-    // categorycollection
-    //  _id
-    //  catName
-    //  // _id = _id
-    //  // catName
-    //  // catAddDate
-    //  catAddDate
     var pageInfo = {
       title: 'Category',
       page: "Dashboard",
@@ -41,8 +34,9 @@ router.get('/', function(req, res, next) {
 });
 
 
-// POST - category - Category root page
+// POST - category - Insert new Category
 router.post('/', function(req, res) {
+
   if (req.session.user == undefined) {
     var msg = "Session is empty";
     sessionName = "Session is Empty";
@@ -63,7 +57,10 @@ router.post('/', function(req, res) {
     //  // catName
     //  // catAddDate
     //  catAddDate
-  if (req.body.catParent == "root") {
+
+  // Add category
+  // Add Parent category - if its select=root
+  if (req.body.catParentName == "root") {
     // root - New cat
     var newData = {
       catName: req.body.catName,
@@ -86,47 +83,47 @@ router.post('/', function(req, res) {
             categoryInfo: results
           });
         });
-      };
+      }
     });
-  } else { // anything else - update
-    // UPDATE
+  } else { // Else add Sub category
+    // Add sub category - if its select=<category>
+    // UPDATE - insert into selected category
     var newData = {
-      catName: req.body.catName,
-      catAddDate: moment().format('MMMM Do YYYY, h:mm:ss a')
-    };
+      subCat: {
+        catName: req.body.catName,
+        catAddDate: moment().format('MMMM Do YYYY, h:mm:ss a')
+      }
+    }
     var updateCondition = {
-      _id: req.body.catParent
-    };
-
+      _id: req.body.catParentName
+    }
     console.log("updateCondition: " + JSON.stringify(updateCondition));
-
-    collection.update({_id: req.body.catParent}, {$push: {subCat: newData}}, {}, function(err, results) {
+    collection.update(updateCondition, {$push: newData}, {},
+      function(err, results) {
       if (err) { // If it failed, return error
         console.log(err);
         res.send("There was a problem updating: " + req.body.catParent);
       } else { // Hey! We Add new one!
         console.log(results);
-
         var pageInfo = {
           title: 'Category',
-          page: "updated!",
+          page: "sub category added!",
           request: "post",
           sessionName: sessionName
         }
         collection.find({}, function(e, results) {
-          console.log("cat been update for: " + req.body.catParent);
           res.render('category/view', { // category/view
             pageInfo: pageInfo,
             categoryInfo: results
           });
         });
-      };
+      }
     });
-  };
+  }
 });
 
-// Remove user
-// GET to remove user/remove Service
+// Remove category
+// GET to remove category/remove
 router.get('/remove', function(req, res) {
   if (req.session.user == undefined) {
     var msg = "Session is empty";
@@ -151,36 +148,69 @@ router.post('/remove', function(req, res) {
     var msg = "Session is active, user: " + req.session.user;
     sessionName = req.session.user;
     console.log(msg);
-  };
+  }
 
   // Set our internal DB variable
   var db = req.db;
   var collection = db.get('categorycollection');
-  var removeCat = {
-    _id: req.body.catID
-  };
-  collection.remove(removeCat, function(err, results) {
-    if (err) {
-      res.send("Problem removing ID: " + removeCat._id);
-      console.log("Problem removing ID: " + removeCat._id);
-    } else {
-      console.log("Info been Removed! ID: " + removeCat._id);
 
-      var pageInfo = {
-        title: 'Category',
-        page: "removed!",
-        request: "post",
-        sessionName: sessionName
-      }
-      collection.find({}, {}, function(e, results) {
+  if (req.body.removeCatSubName){
+    // Remove sub category by $pull
+    var pullData = {
+      catName: req.body.removeCatSubName,
+      catAddDate: req.body.removeCatSubDate
+    }
+    collection.update({_id: req.body.removeCatParentID},
+      {$pull: {"subCat": pullData}},
+       function (err, results) {
+         if (err) { // If it failed, return error
+           console.log(err);
+           res.send("There was a problem removing: " + req.body.removeCatSubName);
+         } else { // Hey! We Add new one!
+           console.log(results);
 
-        res.render('category/view', {
-          pageInfo: pageInfo,
-          categoryInfo: results
+           var pageInfo = {
+             title: 'Category',
+             page: "sub category removed",
+             request: "post",
+             sessionName: sessionName
+           }
+           collection.find({}, function(e, results) {
+             console.log("sub cat been removed: " + req.body.removeCatParentID);
+             res.render('category/view', { // category/view
+               pageInfo: pageInfo,
+               categoryInfo: results
+             });
+           });
+         };
+       });
+   } if (req.body.removeCatID){
+
+    // Remove Category Parent
+    var removeCat = {
+      _id: req.body.removeCatID
+    }
+    collection.remove(removeCat, function(err, results) {
+      if (err) {
+        res.send("Problem removing ID: " + removeCat._id);
+        console.log("Problem removing ID: " + removeCat._id);
+      } else {
+        console.log("\nCat Removed! ID: " + removeCat._id);
+        var pageInfo = {
+          title: 'Category',
+          page: "Category removed!",
+          request: "post",
+          sessionName: sessionName
+        }
+        collection.find({}, {}, function(e, results) {
+          res.render('category/view', {
+            pageInfo: pageInfo,
+            categoryInfo: results
+          });
         });
-      })
-    };
-  });
+      }
+    });
+  }
 });
 
 
