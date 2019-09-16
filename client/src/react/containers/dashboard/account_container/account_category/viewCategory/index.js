20a26b48d5d8533e59f44b7aa72a26958dc4bb59
Danzilla <dan.alphonza@gmail.com>
2019-09-15 23:01:28 -0400

    Going to Create Sample Data | Router? /sampledata - yee

--- client/src/react/containers/dashboard/account_container/account_category/viewCategory/index.js
+++ client/src/react/containers/dashboard/account_container/account_category/viewCategory/index.js
@@ -1,23 +1,55 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
-import { Button, List, Skeleton } from 'antd';
+import { Layout, Button, List, Skeleton } from 'antd';
+import { Tree, Icon } from 'antd';
+
+const { TreeNode } = Tree;
+const { Header, Footer, Sider, Content } = Layout;
 // Account Category View
 function AccountCategoryView(props){
-    // Account Category
+    // Account Category View
     return (
-        <List
-            loading={props.isLoading}
-            itemLayout="horizontal"
-            dataSource={props.fannyAccountCategory}
-            renderItem={item => (
-            <List.Item 
-                actions={[<Button type="link" key="list-loadmore-edit">edit</Button>, <Button type="link" key="list-loadmore-more">more</Button>]}>
-                <Skeleton avatar title={false} loading={props.isLoading} active>
-                    <List.Item.Meta title={<Button type="link">{item.category_id} - {item.category_name} - {item.category_parent} - {item.category_created}</Button>} />
-                </Skeleton>
-            </List.Item>
-            )}
-        />
+        <Content>
+
+            <Tree
+                showIcon
+                // defaultExpandAll
+                defaultSelectedKeys={['0-0']}
+                switcherIcon={<Icon type="down" />}>
+                <TreeNode icon={<Icon type="smile-o" />} title="parent 1" key="0-0">
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-0-0" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-0-1" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-0-2" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-0-3" />
+                </TreeNode>
+                <TreeNode icon={<Icon type="smile-o" />} title="parent 2" key="0-1">
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-1-0" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-1-1" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-1-2" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-1-3" />
+                </TreeNode>
+                <TreeNode icon={<Icon type="smile-o" />} title="parent 3" key="0-2">
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-2-0" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-2-1" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-2-2" />
+                    <TreeNode icon={<Icon type="meh-o" />} title="leaf" key="0-2-3" />
+                </TreeNode>
+            </Tree>
+
+            <List
+                loading={props.isLoading}
+                itemLayout="horizontal"
+                dataSource={props.fannyAccountCategory}
+                renderItem={item => (
+                <List.Item 
+                    actions={[<Button type="link" key="list-loadmore-edit">edit</Button>, <Button type="link" key="list-loadmore-more">more</Button>]}>
+                    <Skeleton avatar title={false} loading={props.isLoading} active>
+                        <List.Item.Meta title={<Button type="link">{item.category_id} - {item.category_name} - {item.category_parent} - {item.category_created}</Button>} />
+                    </Skeleton>
+                </List.Item>
+                )}
+            />
+        </Content>
     );
 }
 export default withRouter(AccountCategoryView);
